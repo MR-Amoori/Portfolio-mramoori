dc48bf750a74e7bc7ffebb42218c62f9b1cbad4d
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-11-09 15:19:15 +0330

    Add Personal Model

---
+++ Portfolio.DataLayer/Models/Personal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.DataLayer.Models
+{
+    class Personal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Languages { get; set; }
+        public string Gender { get; set; }
+        public string Mobile { get; set; }
+        public string Freelancer { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Instagram { get; set; }
+        public string Twitter { get; set; }
+        public string Github { get; set; }
+        public string Linkedin { get; set; }
+
+    }
+}
