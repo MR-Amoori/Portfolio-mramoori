9a2621c59aebdbbf77bbb93ca34a6b75e4161826
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-11-14 23:32:48 +0330

    Delete

---
+++ Portfolio.DataLayer/Repositories/IPortfolioRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Portfolio.DataLayer.Repositories
+{
+    public interface IPortfolioRepository
+    {
+        Models.Portfolio GetBy(int id);
+        void Update(Models.Portfolio portfolio);
+        void Delete(int id);
+        void AddPortfolio(Models.Portfolio portfolio);
+        void Save();
+    }
+}
---
+++ Portfolio/Pages/Admin/Portfolio/AddPortfolio.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.DataLayer.Repositories;
+
+namespace Portfolio.Pages.Admin.Portfolio
+{
+    public class AddPortfolioModel : PageModel
+    {
+        private IPortfolioRepository _repository;
+
+        public AddPortfolioModel(IPortfolioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public DataLayer.Models.Portfolio Portfolio { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(IFormCollection form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+           _repository.AddPortfolio(Portfolio);
+
+            return RedirectToPage("/Admin/Index");
+        }
+    }
+}
