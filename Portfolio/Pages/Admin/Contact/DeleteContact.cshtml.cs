41f3fca8904daa69cc2ca83ee968197278eadcf9
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-11-15 15:13:30 +0330

    Delete Message

---
+++ Portfolio/Pages/Admin/Contact/DeleteContact.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.DataLayer.Repositories;
+
+namespace Portfolio.Pages.Admin.Contact
+{
+    public class DeleteContactModel : PageModel
+    {
+        private IContactRepository _repository;
+
+        public DeleteContactModel(IContactRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public RedirectToPageResult OnGet(int id)
+        {
+            _repository.Delete(id);
+            return RedirectToPage("/Admin/Index");
+        }
+    }
+}
