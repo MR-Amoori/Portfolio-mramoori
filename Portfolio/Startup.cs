fc01cd036889dbd0e2c6effa08c308e84ed042a6
MR-Amoori <mohamad.reza.amoori99@gmail.com>
2026-07-12 17:35:24 +0330

    Version 3: Dynamic About Me section with HTML rendering and Persian encoding support

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -44,7 +44,8 @@ namespace Portfolio
             services.AddScoped<IBlogRepository, BlogRepository>();
 
 
-            string connetcionString = Configuration.GetConnectionString("Production");
+            // string connetcionString = Configuration.GetConnectionString("Production");
+            string connetcionString = Configuration.GetConnectionString("LocalHost");
 
             services.AddDbContext<PortfolioContext>(options => options.UseSqlServer(connetcionString));
 
